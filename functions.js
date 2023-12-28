537285317621914a25d5454ef7c499184b3db04e
koldovsky <koldovsky@gmail.com>
2023-12-28 19:57:42 +0200

    Function calling

---
+++ functions.js
@@ -0,0 +1,97 @@
+import { openai } from './openai.js'
+import math from 'advanced-calculator'
+const QUESTION = process.argv[2] || 'hi'
+
+const messages = [
+  {
+    role: 'user',
+    content: QUESTION,
+  },
+]
+
+const functions = {
+  calculate: async ({ expression }) => {
+    return math.evaluate(expression)
+  },
+  generateImage: async ({ prompt }) => {
+    const result = await openai.images.generate({ prompt })
+    console.log(result)
+    return ''
+  },
+}
+
+const getCompletion = async (messages) => {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-0613',
+    messages,
+    functions: [
+      {
+        name: 'calculate',
+        description: 'Run a math expression',
+        parameters: {
+          type: 'object',
+          properties: {
+            expression: {
+              type: 'string',
+              description:
+                'Then math expression to evaluate like "2 * 3 + (21 / 2) ^ 2"',
+            },
+          },
+          required: ['expression'],
+        },
+      },
+      {
+        name: 'generateImage',
+        description: 'Generate an image from a prompt',
+        parameters: {
+          type: 'object',
+          properties: {
+            prompt: {
+              type: 'string',
+              description:
+                'The prompt to generate an image from like "A cat in a field"',
+            },
+          },
+          required: ['prompt'],
+        },
+      },
+    ],
+    temperature: 0,
+  })
+
+  return response
+}
+
+let response
+while (true) {
+  response = await getCompletion(messages)
+
+  if (response.choices[0].finish_reason === 'stop') {
+    console.log(response.choices[0].message.content)
+    break
+  } else if (response.choices[0].finish_reason === 'function_call') {
+    const fnName = response.choices[0].message.function_call.name
+    const args = response.choices[0].message.function_call.arguments
+    console.log(fnName, args)
+
+    const functionToCall = functions[fnName]
+    const params = JSON.parse(args)
+
+    const result = await functionToCall(params)
+
+    messages.push({
+      role: 'assistant',
+      content: null,
+      function_call: {
+        name: fnName,
+        arguments: args,
+      },
+    })
+
+    messages.push({
+      role: 'function',
+      name: fnName,
+      content: JSON.stringify({ result: result }),
+    })
+  }
+}
